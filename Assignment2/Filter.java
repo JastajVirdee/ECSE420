e02a08a78fe51e1c87f4e4525aa5f4be8c3ad4ed
Spimav <spirosmavroidakos@gmail.com>
2018-11-01 19:12:22 -0400

    fixed errors

--- Assignment2/Filter.java
+++ Assignment2/Filter.java
@@ -1,8 +1,9 @@
 import java.util.concurrent.locks.Lock;
+import java.lang.*;
 
 class Filter implements Lock{ 
-    private int level[] = new int[n];
-    private int victim[] = new int[n];
+    private int level[];
+    private int victim[];
     
     /**
      * Constructor for the filter class
@@ -21,13 +22,13 @@ class Filter implements Lock{
     /**
      * Method to lock the lock
      */
-    public void Lock(){
-        int threadId = ThreadId.get();
+    public void lock(){
+        int threadId = (int) Thread.currentThread().getId() % level.length;
         for (int L = 1; L < level.length; L++){
             level[threadId] = L;
             victim[L] = threadId;
 
-            for (int k = 0; k < n; k++) {
+            for (int k = 0; k < level.length; k++) {
                 while ((k != threadId) && (level[k] >= L 
                 && victim[L] == threadId)){};
             }
@@ -37,8 +38,27 @@ class Filter implements Lock{
     /**
      * Method to release the lock
      */
-    public void Release(){
-        int threadId = ThreadId.get();
+    public void unlock(){
+        int threadId = (int) Thread.currentThread().getId() % level.length;
         level[threadId] = 0;
+    }
+
+    /*
+    * Abstract methods that need to be defined. They do nothing basically since they 
+    * is no the focus of the filter lock
+    */
+    public java.util.concurrent.locks.Condition newCondition() {
+        return null;
+    }
+
+    public boolean tryLock(long timeout, java.util.concurrent.TimeUnit unit) throws java.lang.InterruptedException {
+        return false;
+    }
+
+    public boolean tryLock() {
+        return false;
+    }
+
+    public void lockInterruptibly() throws java.lang.InterruptedException {
     }
 }
